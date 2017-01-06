36e33dc2684b653d7ab513cf48e84a6da6ec6cc2
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-01-06 17:37:28 +0800

    go

---
+++ HelloGo/Hello.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+
+var a = "w3cschool菜鸟教程"
+var b string = "w3cschool.cc"
+var c bool
+
+var d=10
+
+
+var a1, b1, c1 int
+
+func main() {
+
+	v3 := 8
+	v4 := 8.0
+	m:=10
+	fmt.Println("hello word")
+
+	fmt.Println("Hello, World!")
+	fmt.Println("w3cschool菜鸟教程：w3cschool.cc")
+
+	a1, b1, c1 = 5, 7, 8
+
+	println(a,b,c,d,v3,v4)
+	println(m,a1,b1,c1)
+
+}
